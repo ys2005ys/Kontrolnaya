c1d669a8be4dff940caae54d61a1c216e6d41e55
ys2005ys <ys2005@mail.ru>
2023-03-21 00:07:55 +0300

    add code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿void ResultArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}" + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string[4] {"hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+ResultArray(array1, array2);
+ShowArray(array1);
+ShowArray(array2);
+
